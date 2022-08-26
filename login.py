369f1080f8074fee9d1521bd4e19663d7f8ea6b8
Sai Shibu N B <saishibu38380@gmail.com>
2022-08-26 16:00:40 +0530

    Update login.py

--- login.py
+++ login.py
@@ -99,17 +99,7 @@ def do_admin_login():
     
 
 
-# @app.route('/register',methods=['POST'])
-# def do_register():
-#     return render_template('donation.html')
-
-# @app.route('/postRegister',methods=['POST'])
-# def save_register():
-#     username = request.form['username'] 
-#     password = request.form['password']
-#     confirmpassword = request.form['confirmpassword']
-
-   
+  
 
 
 if __name__ == "__main__":
@@ -121,91 +111,4 @@ if __name__ == "__main__":
     sess.init_app(app)
 
     app.debug = True
-    app.run(host="0.0.0.0",port=5000)
-    
-    
-
-
-
-# @app.route('/AARTestrev', methods=['POST'])
-# def AARTestrev():
-# 	try:
-# 		GPIO.setup(17,GPIO.HIGH)
-# 		time.sleep(10)#FWD
-# 		GPIO.setup(17,GPIO.LOW)
-# 		flash ('AAR Test Completed')
-# 	except:
-# 		flash("Error Testing")
-# 	return redirect(url_for('mainPage'))
-
-# @app.route('/update', methods=['POST'])
-# def update():
-#     try:
-#         path='/home/pi/OceanNet/AAR/ConfigPage/'
-#         os.chdir(path)
-#         cmd="git pull"
-#         os.system(cmd)
-#         flash ('Update Completed')
-#     except:
-#         flash("Error Software Updation")
-#     return redirect(url_for('mainPage'))
-
-# @app.route('/reboot', methods=['POST'])
-# def reboot():
-    
-#     cmd="reboot"
-#     os.system(cmd)
-    
-#     return redirect(url_for('mainPage'))
-
-# @app.route('/LEDTest1', methods=['POST'])#status
-# def LEDTest1():
-#     try:
-#         cmd="/home/pi/OceanNet/AAR/ConfigPage/LEDtest.py"
-#         os.system(cmd)
-#         flash ('LED Test Completed')
-#     except:
-#         flash("Error Testing Notification LED")
-#     return redirect(url_for('mainPage'))
-
-# # app.route('/LEDTest2', methods=['POST'])#rssi1
-# # def LEDTest2():
-# # 	try:
-# # 		GPIO.setup(19,GPIO.HIGH)
-# # 		time.sleep(2)#FWD
-# # 		GPIO.setup(19,GPIO.LOW)
-# # 		flash ('RSSI1 Test Completed')
-# # 	except:
-# # 		flash("Error Testing Notification LED")
-# # 	return redirect(url_for('mainPage'))
-# # @app.route('/LEDTest3', methods=['POST'])#rssi2
-# # def LEDTest3():
-# # 	try:
-# # 		GPIO.setup(13,GPIO.HIGH)
-# # 		time.sleep(2)#FWD
-# # 		GPIO.setup(13,GPIO.LOW)
-# # 		flash ('RSSI2 Test Completed')
-# # 	except:
-# # 		flash("Error Testing Notification LED")
-# # 	return redirect(url_for('mainPage'))
-# # @app.route('/LEDTest4', methods=['POST'])#rssi3
-# # def LEDTest4():
-# # 	try:
-# # 		GPIO.setup(6,GPIO.HIGH)
-# # 		time.sleep(2)#FWD
-# # 		GPIO.setup(6,GPIO.LOW)
-# # 		flash ('RSSI3 Test Completed')
-# # 	except:
-# # 		flash("Error Testing Notification LED")
-# # 	return redirect(url_for('mainPage'))
-# # @app.route('/LEDTest5', methods=['POST'])#rssi4
-# # def LEDTest5():
-# # # 	try:
-# # 	GPIO.setup(5,GPIO.HIGH)
-# # 	time.sleep(2)#FWD
-# # 	GPIO.setup(5,GPIO.LOW)
-# # 	flash ('RSSI1 Test Completed')
-# # 	except:
-# # 		flash("Error Testing Notification LED")
-#     return redirect(url_for('mainPage'))
-# app.run(debug=False,host="0.0.0.0",port="1000", threaded=True)
+    app.run(host="0.0.0.0",port=1000)
